d00ccb6de362cb493bc64b6aa017a711c9806e09
Ivan Heinzer <ivan.heinzer@hotmail.ch>
2021-01-10 22:10:21 +0100

    set date everywhere

--- server/users/controllers/data.controller.js
+++ server/users/controllers/data.controller.js
@@ -34,7 +34,7 @@ exports.newReport = (req, res) => {
         if (req.body["troops"]["attack_troops"][2] > 1000 ||
             req.body["troops"]["attack_troops"][4] > 900) 
         {
-            attacker_type = "OFF";
+            attacker_type = "OFF "  + formatDate(d);
 
             // Rote off?
             if (req.body["troops"]["attack_troops"][2] > 3500 &&
@@ -59,7 +59,7 @@ exports.newReport = (req, res) => {
         if (req.body["troops"]["attack_troops"][2] > 1000 &&
             req.body["troops"]["attack_troops"][5] > 200) 
         {
-            attacker_type = "OFF SKAV";
+            attacker_type = "OFF SKAV " + formatDate(d);
 
 
             // Rote off?
@@ -85,12 +85,12 @@ exports.newReport = (req, res) => {
         // Flexdeff
         if (req.body["troops"]["attack_troops"][0] > 1000 &&
             req.body["troops"]["attack_troops"][5] > 400) {
-            attacker_type = "Flexdeff";
+            attacker_type = "Flexdeff " + formatDate(d);
         }
 
         // Spy Dorf
         if (req.body["troops"]["attack_troops"][3] > 5000) {
-            attacker_type = "Only Spy";
+            attacker_type = "Only Spy " + formatDate(d);
         } 
 
         if (attacker_type != false){
@@ -136,11 +136,11 @@ exports.newReport = (req, res) => {
 
         if ("outside_troops" in req.body["troops"]) {
             if (req.body["troops"]["outside_troops"][3] > 5000) {
-                defender_type = "Only Spy";
+                defender_type = "Only Spy " + formatDate(d);
             } 
 
             if (req.body["troops"]["outside_troops"][2] > 1000) {
-                defender_type= "OFF";
+                defender_type= "OFF " + formatDate(d);
 
                 if (req.body["troops"]["outside_troops"][2] > 4000 &&
                     req.body["troops"]["outside_troops"][4] > 2000
@@ -151,16 +151,16 @@ exports.newReport = (req, res) => {
 
             if (req.body["troops"]["outside_troops"][2] > 1000 &&
                 req.body["troops"]["outside_troops"][5] > 200) {
-                defender_type= "OFF SKAV";
+                defender_type= "OFF SKAV " + formatDate(d);
             }
 
             if (req.body["troops"]["outside_troops"][1] > 2000) {
-                defender_type= "DEFF SD";
+                defender_type= "DEFF SD " + formatDate(d);
             }
             
             if (req.body["troops"]["outside_troops"][0] > 2500 &&
                 req.body["troops"]["outside_troops"][5] > 400) {
-                defender_type= "Flexdeff";
+                defender_type= "Flexdeff " + formatDate(d);
             }
 
         }
